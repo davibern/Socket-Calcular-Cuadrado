a50562fac9c32ecb1869c0987e3ff4444aa16735
davibern <david@Lorien.localdomain>
2021-01-15 20:23:20 +0100

    Version 1.1

--- src/davibern/tcp/Client.java
+++ src/davibern/tcp/Client.java
@@ -1,5 +1,6 @@
 package davibern.tcp;
 
+// Librerías
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -7,32 +8,60 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+/**
+ * Cliente que envía un número para que el servidor calcule su raíz cuadrada
+ * 
+ * @author davibern
+ * @version 1.1
+ */
 public class Client {
     
+    // Atributos
     private final String HOST = "localhost";
     private final int PORT = 5555;
     private int square;
     
+    /**
+     * Constructor de clase. Se inicializará con un número de tipo entero
+     * 
+     * @param square Valor del que se calculará su raíz cuadrada
+     */
     public Client(int square) {
         
         this.square = square;
         
         try {
+            /*
+                - Se crea el socket del cliente con el host y puerto especificado
+                - Se abre un stream de datos de tipo entrada
+                - Se abre un stream de datos de tiop salida
+            */
             Socket client = new Socket(this.HOST, this.PORT);
             InputStream input = client.getInputStream();
             DataInputStream read = new DataInputStream(input);
             System.out.println(read.readUTF());
             
+            /*
+                Se inicializa los streams de entrada y salida
+                Se recupera del servidor el valor de la raíz cuadrada
+                Se muestra por pantalla
+                Se cierra el socket (cliente)
+            */
             OutputStream output = client.getOutputStream();
             DataOutputStream write = new DataOutputStream(output);
             write.writeUTF("" + this.square);
             System.out.println(read.readUTF());
+            client.close();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
         
     }
     
+    /**
+     * Método que ejecuta Client
+     * @param args Sin argumentos
+     */
     public static void main (String[] args) {
         new Client(16);
     }
--- src/davibern/tcp/Server.java
+++ src/davibern/tcp/Server.java
@@ -1,5 +1,6 @@
 package davibern.tcp;
 
+// Librerias
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,45 +9,71 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Servidor que recibe un número para calcular su raíz cuadrada
+ * 
+ * @author davibern
+ * @version 1.1
+ */
 public class Server {
     
+    // Atributos
     private final int PORT = 5555;
     private double square;
     
+    /**
+     * Constructor sin parámetros
+     */
     public Server() {
         
         try {
+            // Se inicia el servidor con los parámetros de host y puerto especificados en sus atributos
             ServerSocket server = new ServerSocket(this.PORT);
             System.out.println("Levantado servidor en el puerto " + this.PORT);
             
+            // Se inicia conexión con el cliente
             Socket client = server.accept();
             System.out.println("Conectado el cliente");
             
+            // Se envia información al cliente
             OutputStream output = client.getOutputStream();
             DataOutputStream write = new DataOutputStream(output);
             write.writeUTF("Mensaje enviado desde el servidor");
             
+            // Se obtiene información del servidor, en este caso el valor para calcular
             InputStream input = client.getInputStream();
             DataInputStream read = new DataInputStream(input);
+            // Se castea el valor de entero a doble
             this.square = Double.parseDouble(read.readUTF());
             
+            // Se limpia el outputstream
             write.flush();
             
+            // Se muestra por pantalla y se envía al cliente la raíz cuadrada calculada en getSquare()
             System.out.println("El cuadrado de " + this.square + " es " + this.getSquare());
             write.writeUTF("El cuadrado de " + this.square + " es " + this.getSquare());
-            client.close();
+            // Se servidor
+            server.close();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
         
     }
     
+    /**
+     * Método que calcula la raíz cuadrada
+     * @return Devuelve un doble con la raíz cuadrada
+     */
     private double getSquare() {
         double result;
         result = Math.sqrt(this.square);
         return result;
     }
     
+    /**
+     * Método que ejecuta el programa Server
+     * @param args Sin argumentos
+     */
     public static void main (String[] args) {
         new Server();
     }
